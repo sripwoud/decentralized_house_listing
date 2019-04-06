6fce31c6237662a98b4efb90cc8369a5a5742e95
Gry0u <gauthriou@protonmail.com>
2019-04-06 17:48:21 +0200

    feat: Create child custom token contract

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -514,11 +514,15 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
 }
 
-//  TODO's: Create CustomERC721Token contract that inherits from the ERC721Metadata contract. You can name this contract as you please
-//  1) Pass in appropriate values for the inherited ERC721Metadata contract
-//      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
-//  2) create a public mint() that does the following:
-//      -can only be executed by the contract owner
-//      -takes in a 'to' address, tokenId, and tokenURI as parameters
-//      -returns a true boolean upon completion of the function
-//      -calls the superclass mint and setTokenURI functions
+
+contract DecentralizedHousingToken is ERC721Metadata("Decentralized Housing Token", "DHT", "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") {
+    function mint(address to, uint tokenId)
+    public
+    onlyOwner
+    returns(bool)
+    {
+        _mint(to, tokenId);
+        setTokenURI(tokenID);
+        return true;
+    }
+}
