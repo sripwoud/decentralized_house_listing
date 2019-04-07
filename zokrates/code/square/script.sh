f992bd9679a37d68d337ef4318bd2c27fb95ac90
sripwoud <me@sripwoud.xyz>
2019-04-07 15:01:02 +0200

    feat: Generate multiple ZoKrates proofs using shell script

---
+++ zokrates/code/square/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "[" > proofs.json
+for ((i=1;i<=9;i++))
+  do
+    ~/zokrates compute-witness -a $i `expr $i \* $i`
+    ~/zokrates generate-proof
+    cat proof.json >> proofs.json
+    echo "," >> proofs.json
+  done
+  ~/zokrates compute-witness -a 10 100
+  ~/zokrates generate-proof
+  cat proof.json >> proofs.json
+  echo "]" >> proofs.json
