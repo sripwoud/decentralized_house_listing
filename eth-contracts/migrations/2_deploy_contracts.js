f7d88411bbf793baf807670ad6a960a54b503987
sripwoud <me@sripwoud.xyz>
2019-04-07 15:37:35 +0200

    feat: Mint 10 tokens at migration

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,37 @@
-// migrating the appropriate contracts
-// const SquareVerifier = artifacts.require('./SquareVerifier.sol')
-// const SolnSquareVerifier = artifacts.require('./SolnSquareVerifier.sol')
+const SquareVerifier = artifacts.require('./Verifier.sol')
+const SolnSquareVerifier = artifacts.require('./SolnSquareVerifier.sol')
 const ERC721Mintable = artifacts.require('DecentralizedHousingToken')
+const fs = require('fs')
+const path = require('path')
+const proofs = require('../../zokrates/code/square/proofs.json')
 
-module.exports = function (deployer) {
-  deployer.deploy(ERC721Mintable)
-  // deployer.deploy(SquareVerifier)
-  // deployer.deploy(SolnSquareVerifier)
+module.exports = function (deployer, network, accounts) {
+  // deployer.deploy(ERC721Mintable)
+  deployer.deploy(SquareVerifier)
+    .then(instance => {
+      return deployer.deploy(SolnSquareVerifier, SquareVerifier.address)
+        .then(instance => {
+          // mint tokens
+          proofs.forEach((proof, index) => {
+            instance.mintNewToken(
+              accounts[1],
+              index,
+              proof.proof.A,
+              proof.proof.A_p,
+              proof.proof.B,
+              proof.proof.B_p,
+              proof.proof.C,
+              proof.proof.C_p,
+              proof.proof.H,
+              proof.proof.K,
+              proof.input
+            ).then(val => {
+              if (val) {
+                console.log()
+                console.log(`token ${index} minted to ${accounts[1]}`)
+              }
+            })
+          })
+        })
+    })
 }
