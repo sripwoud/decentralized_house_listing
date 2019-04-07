af55f75abc6ec46be06c49072bb69901f157a7c4
sripwoud <me@sripwoud.xyz>
2019-04-07 21:55:08 +0200

    Deploy on rinkeby

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -60,7 +60,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
       network_id: 4,
       confirmations: 2,
       timeoutBlocks: 200,
